938f7fe731fa1b77ffd0b3e2dab1e2d77d662288
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-12 20:52:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  var lowerCase = string.toLowerCase;
+  if(string === lowerCase) {
+    return "I can\'t hear you!";
+  }
+  return string;
+}
